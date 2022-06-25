297c8a60653ad4504bb19a14e023343cb92fe38b
Maicon Costa <maiconcostawd@hotmail.com>
2022-06-25 11:18:58 -0300

    feat: create loading component

---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,18 @@
+import { FC } from 'react';
+import './styles.scss';
+
+export const Loading: FC = () => {
+  return (
+    <div className="lds-grid">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
