e44d555a785aa0e365e15421da74f79ec8365cb0
Maicon Costa <maiconcostawd@hotmail.com>
2022-06-25 11:45:45 -0300

    feat: create scrollToTop component

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,14 +2,17 @@ import { Route, Routes } from 'react-router-dom';
 import { Header } from '../components';
 import Home from '../pages/Home';
 import Movie from '../pages/Movie';
+import ScrollToTop from './ScrollToTop';
 
 const RoutesComponent = () => (
   <>
     <Header />
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/movie/:movieId" element={<Movie />} />
-    </Routes>
+    <ScrollToTop>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movie/:movieId" element={<Movie />} />
+      </Routes>
+    </ScrollToTop>
   </>
 );
 
