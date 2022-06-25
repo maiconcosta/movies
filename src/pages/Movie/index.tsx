bb5cbc8acfa96587a6309984263997f7d867cef0
Maicon Costa <maiconcostawd@hotmail.com>
2022-06-25 11:29:15 -0300

    feat: add Movie page

---
+++ src/pages/Movie/index.tsx
@@ -0,0 +1,106 @@
+import { format } from 'date-fns';
+
+import { useParams } from 'react-router-dom';
+import { useMovie } from '../../services/Movie';
+import { useTrailer } from '../../services/Trailer';
+
+import { Label } from '../../components';
+
+import noPhoto from '../../assets/images/no-photo.png';
+import './styles.scss';
+
+const Movie = () => {
+  const params = useParams();
+
+  const { data: movie } = useMovie(params?.movieId, {
+    refetchOnWindowFocus: false,
+  });
+  const { data: trailer } = useTrailer(params?.movieId, {
+    refetchOnWindowFocus: false,
+  });
+
+  const imagePath = movie?.poster_path
+    ? `https://www.themoviedb.org/t/p/w300_and_h450_bestv2/${movie.poster_path}`
+    : noPhoto;
+
+  const currencyFormatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+  });
+
+  return (
+    <>
+      <div className="container">
+        <div className="title">
+          <h2>{movie?.title}</h2>
+          <h4>
+            {movie?.release_date
+              ? format(new Date(movie.release_date), 'dd/MM/yyyy')
+              : 'Data de lançamento desconhecida'}
+          </h4>
+        </div>
+        <div className="detail">
+          <div className="info">
+            <h3>Sinopse</h3>
+            <p>
+              {movie?.overview
+                ? movie?.overview
+                : 'Nenhuma sinopse disponível para esse filme.'}
+            </p>
+
+            <h3>Informações</h3>
+
+            <div className="movieInfos">
+              <div>
+                <h4>Situação</h4>
+                <p>{movie?.status}</p>
+              </div>
+              <div>
+                <h4>Idioma</h4>
+                <p>{movie?.original_language}</p>
+              </div>
+              <div>
+                <h4>Duração</h4>
+                <p>{movie?.runtime}</p>
+              </div>
+              <div>
+                <h4>Orçamento</h4>
+                <p>{currencyFormatter.format(movie?.budget || 0)}</p>
+              </div>
+              <div>
+                <h4>Receita</h4>
+                <p>{currencyFormatter.format(movie?.revenue || 0)}</p>
+              </div>
+              <div>
+                <h4>Lucro</h4>
+                <p>
+                  {movie?.revenue &&
+                    currencyFormatter.format(
+                      movie?.revenue - movie?.budget || 0,
+                    )}
+                </p>
+              </div>
+            </div>
+
+            {movie?.genres.map((genre) => (
+              <Label>{genre.name}</Label>
+            ))}
+          </div>
+
+          <img src={imagePath} alt={movie?.title} />
+        </div>
+      </div>
+
+      {trailer && trailer?.results?.length > 0 && (
+        <iframe
+          id="ytplayer"
+          width="100%"
+          height="600px"
+          src={`http://www.youtube.com/embed/${trailer?.results[0].key}?autoplay=0`}
+        />
+      )}
+    </>
+  );
+};
+
+export default Movie;
